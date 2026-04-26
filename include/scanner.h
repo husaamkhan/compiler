cfd88e4842516f21ba7874a3606bb5fdc781cfaf
Husaam Khan <husaamkhan04@gmail.com>
2026-04-26 11:54:23 -0400

    Added scanner class
    not much added yet
    - enums for states and tokens
    - constructor destructor
    - init, scan function (scan loop so far only reads next char)
    - nextchar and fill buffer

--- include/scanner.h
+++ include/scanner.h
@@ -1,16 +1,47 @@
+#include <iostream>
+
 #ifndef SCANNER
 #define SCANNER
 
-#include <string>
+#define BUFFER_LENGTH 4096
 
-using namespace std;
+enum State { ERROR, ACCEPTING, NON_ACCEPTING };
+enum Token
+{
+	INCOMPLETE,
+	IDENTIFIER,
+	BOOLEAN,
+	NUMBER,
+	CHARACTER,
+	STRING,
+	L_PARENTHESIS,
+	R_PARENTHESIS,
+	VECTOR_START,
+	QUOTE,
+	QUASIQUOTE,
+	UNQUOTE,
+	UNQUOTE_SPLICE,
+	DOT
+};
 
 class Scanner
-{
+{ 
 	public:
 		Scanner();
 		~Scanner();
-		
-		void scan(string);
+
+		void scan();
+		void init(std::istream* stream);
+
+	private:
+		void fillBuffer(int pos);
+		char nextChar();
+
+		std::istream* input_stream;
+
+		char double_buffer[2 * BUFFER_LENGTH];
+		int cur_pos;
+		int fence; // Used to prevent rollback into incorrect buffer
+
 };
 #endif
--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,16 +1,54 @@
 #include "scanner.h"
-#include <iostream>
 
-using namespace std;
-
-Scanner::Scanner()
+Scanner::Scanner() : input_stream(nullptr)
 {}
 
 Scanner::~Scanner()
 {}
 
-void Scanner::scan(string input)
+void Scanner::init(std::istream* stream)
 {
-	cout << "Hello from scanner!";
+	input_stream = stream;
+	int cur_pos = 0;
+}
+
+void Scanner::fillBuffer(int pos)
+{
+	input_stream->read(double_buffer + pos, BUFFER_LENGTH);
+}
+
+char Scanner::nextChar()
+{
+	char c = this->double_buffer[this->cur_pos];
+	
+	if (c != EOF)
+	{
+		this->cur_pos = (this->cur_pos + 1) % 2*BUFFER_LENGTH;
+
+		// Fills next buffer if current position has crossed into it and updates fence
+		if ((cur_pos % BUFFER_LENGTH) == 0)
+		{
+			this->fillBuffer(cur_pos);
+			this->fence = (cur_pos + BUFFER_LENGTH) % 2*BUFFER_LENGTH;
+		}
+	}
+
+	return c;
+}
+
+void Scanner::scan()
+{
+	input_stream->read(double_buffer, BUFFER_LENGTH);
+	this->fillBuffer(0);
+
+	std::string lexeme = "";
+	Token token = INCOMPLETE;
+
+	State state = NON_ACCEPTING; 
+
+	while (state != ERROR)
+	{
+		char cur = nextChar();
+	}
 }
 
